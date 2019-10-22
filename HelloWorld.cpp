43bf59c3b5b1b39af8c71ef21520bc6b37625c55
Akshath Kaushal <42678785+akshathkaushal@users.noreply.github.com>
2019-10-23 00:07:47 +0530

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,4 +3,5 @@ using namespace std;
 int main()
 {
   cout<<"Hello World!!!"<<endl;
+  cout<<"I am Aman Kumar. I am presently studying CSE in IIIT Bangalore. I completed my 10th from DPS-Vizag and 12th from FIITJEE-Vizag. I am interested in Competetive programming and Web-development."<<endl;
 }
